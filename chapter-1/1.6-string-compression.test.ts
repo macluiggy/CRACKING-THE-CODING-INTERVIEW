5865ae951829776482d94d1c68c36b7e3ae6a506
macluiggy <luiggy.macias@imagineapps.co>
2023-11-15 11:22:35 -0500

    Add string compression functions to 1.6-string-compression module

--- chapter-1/1.6-string-compression.test.ts
+++ chapter-1/1.6-string-compression.test.ts
@@ -1,14 +1,24 @@
 import { expect, describe, it } from "bun:test";
-import { stringCompression } from "./1.6-string-compression";
+import {
+  stringCompression,
+  compress,
+  compress2,
+  compressBad,
+} from "./1.6-string-compression";
 
 describe("1.6 - String Compression", () => {
+  const fns = [stringCompression, compress, compress2, compressBad];
   it("should compress the string correctly", () => {
-    expect(stringCompression("aabcccccaaa")).toEqual("a2b1c5a3");
-    expect(stringCompression("abcde")).toEqual("abcde");
-    expect(stringCompression("aaabbbccc")).toEqual("a3b3c3");
+    for (let fn of fns) {
+      expect(fn("aabcccccaaa")).toEqual("a2b1c5a3");
+      expect(fn("abcde")).toEqual("abcde");
+      expect(fn("aaabbbccc")).toEqual("a3b3c3");
+    }
   });
   it("should return the original string if the compressed string is not shorter", () => {
-    expect(stringCompression("abcd")).toEqual("abcd");
-    expect(stringCompression("aabbccdd")).toEqual("aabbccdd");
+    for (let fn of fns) {
+      expect(fn("abcd")).toEqual("abcd");
+      expect(fn("aabbccdd")).toEqual("aabbccdd");
+    }
   });
 });
--- chapter-1/1.6-string-compression.ts
+++ chapter-1/1.6-string-compression.ts
@@ -13,4 +13,70 @@ const stringCompression = (str: string) => {
   return newStr.length >= str.length ? str : newStr;
 };
 
-export { stringCompression };
+function compress(str: string): string {
+  let finalLength = countCompression(str);
+  if (finalLength >= str.length) return str;
+
+  let compressed: string[] = [];
+  let countConsecutive = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    countConsecutive++;
+    if (i + 1 >= str.length || str.charAt(i) !== str.charAt(i + 1)) {
+      compressed.push(str.charAt(i));
+      compressed.push(countConsecutive.toString());
+      countConsecutive = 0;
+    }
+  }
+
+  return compressed.join("");
+}
+
+function countCompression(str: string): number {
+  let compressedLength = 0;
+  let countConsecutive = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    countConsecutive++;
+    if (i + 1 >= str.length || str.charAt(i) !== str.charAt(i + 1)) {
+      compressedLength += 1 + countConsecutive.toString().length;
+      countConsecutive = 0;
+    }
+  }
+
+  return compressedLength;
+}
+
+function compress2(str: string): string {
+  let compressed: string[] = [];
+  let countConsecutive = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    countConsecutive++;
+    if (i + 1 >= str.length || str.charAt(i) !== str.charAt(i + 1)) {
+      compressed.push(str.charAt(i));
+      compressed.push(countConsecutive.toString());
+      countConsecutive = 0;
+    }
+  }
+
+  let compressedStr = compressed.join("");
+  return compressedStr.length < str.length ? compressedStr : str;
+} // O(n)
+
+function compressBad(str: string): string {
+  let compressedString = "";
+  let countConsecutive = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    countConsecutive++;
+    if (i + 1 >= str.length || str.charAt(i) !== str.charAt(i + 1)) {
+      compressedString += "" + str.charAt(i) + countConsecutive;
+      countConsecutive = 0;
+    }
+  }
+
+  return compressedString.length < str.length ? compressedString : str;
+}
+
+export { stringCompression, compress, compress2, compressBad };
