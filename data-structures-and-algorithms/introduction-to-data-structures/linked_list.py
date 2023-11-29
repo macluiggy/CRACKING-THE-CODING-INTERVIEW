8581b392cce9e88650582ceb90522c44588754eb
macluiggy <luiggy.macias@imagineapps.co>
2023-11-29 12:18:22 -0500

    Add search, insert, and remove methods to LinkedList class

--- data-structures-and-algorithms/introduction-to-data-structures/linked_list.py
+++ data-structures-and-algorithms/introduction-to-data-structures/linked_list.py
@@ -71,7 +71,69 @@ class LinkedList:
             current = current.next
         
         return "-> ".join(nodes)
+    
+    def search(self, key):
+        """
+        Search for the first node containing data that matches the key
+        Return the node or None if not found
+        Takes O(n) time
+        """
+        current = self.head
 
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next
+        
+        return None
+    
+    def insert(self, data, index):
+        """
+        Inserts a new Node containing data at index position
+        Insertion takes O(1) time but finding the node at the insertion point takes O(n) time
+        Takes overall O(n) time
+        """
+        if index == 0:
+            self.add(data)
+        
+        if index > 0:
+            new = Node(data)
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = current.next
+                position -= 1
+            
+            prev = current
+            next = current.next
+
+            prev.next = new
+            new.next = next
+    
+    def remove(self, key):
+        """
+        Removes Node containing data that matches the key
+        Returns the node or None if key doesn't exist
+        Takes O(n) time
+        """
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next
+            elif current.data == key:
+                found = True
+                previous.next = current.next
+            else:
+                previous = current
+                current = current.next
+        
+        return current
 
 # N1 = Node(10)
 # print(N1.__repr__())
@@ -83,5 +145,11 @@ l = LinkedList()
 l.add(10)
 l.add(20)
 l.add(30)
-print(l.size())
-print(l.__repr__())
+# l.add({'name': 'John'})
+# print(l.size())
+# print(l.__repr__())
+print(l.search(20))
+print(l.search(50))
+# print(l)
+l.insert(15, 2)
+print(l)
