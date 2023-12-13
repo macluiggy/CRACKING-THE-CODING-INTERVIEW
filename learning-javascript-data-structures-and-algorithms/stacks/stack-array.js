861ceaba45f8aef4392cb360878594d0ef0ece6e
macluiggy <luiggy.macias@imagineapps.co>
2023-12-13 08:14:59 -0500

    Add stack operations and test cases

--- learning-javascript-data-structures-and-algorithms/stacks/stack-array.js
+++ learning-javascript-data-structures-and-algorithms/stacks/stack-array.js
@@ -27,3 +27,17 @@ class Stack {
     this.items = [];
   }
 }
+
+// Path: learning-javascript-data-structures-and-algorithms/stacks/stack-array.js
+const stack = new Stack();
+console.log(stack.isEmpty()); // outputs true
+stack.push(5);
+stack.push(8);
+console.log(stack.peek()); // outputs 8
+stack.push(11);
+console.log(stack.size()); // outputs 3
+console.log(stack.isEmpty()); // outputs false
+stack.push(15);
+stack.pop();
+stack.pop();
+console.log(stack.size()); // outputs 2
