b6187ca735e797361365db309bbfe18e16362c1a
macluiggy <luiggy.macias@imagineapps.co>
2023-12-13 20:48:03 -0500

    Add test case for decimalToBinary function

--- learning-javascript-data-structures-and-algorithms/stacks/decimal-to-binary.test.js
+++ learning-javascript-data-structures-and-algorithms/stacks/decimal-to-binary.test.js
@@ -5,6 +5,7 @@ describe("decimalToBinary", () => {
     expect(decimalToBinary(233)).toBe("11101001");
     expect(decimalToBinary(10)).toBe("1010");
     expect(decimalToBinary(1000)).toBe("1111101000");
+    expect(decimalToBinary(11.1)).toBe("1011");
     expect(Math.floor(5%2)).toBe(1);
     expect(Math.floor(5/2)).toBe(2);
     expect(Math.floor(2%2)).toBe(0);
