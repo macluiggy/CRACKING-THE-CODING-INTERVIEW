4ab6bd35487edec6f66e9c226252a3b39a67d865
macluiggy <luiggy.macias@imagineapps.co>
2023-12-13 20:45:50 -0500

    Refactor decimalToBinary test cases

--- learning-javascript-data-structures-and-algorithms/stacks/decimal-to-binary.test.js
+++ learning-javascript-data-structures-and-algorithms/stacks/decimal-to-binary.test.js
@@ -1,19 +1,15 @@
 import { decimalToBinary } from "./decimal-to-binary";
 
-// Test case 1: Positive decimal number
-console.log(decimalToBinary(10)); // Expected output: "1010"
-
-// Test case 2: Zero
-console.log(decimalToBinary(0)); // Expected output: "0"
-
-// Test case 3: Negative decimal number
-console.log(decimalToBinary(-5)); // Expected output: "-101"
-
-// Test case 4: Large decimal number
-console.log(decimalToBinary(123456789)); // Expected output: "111010110111100110100010101"
-
-// Test case 5: Decimal number with fractional part
-console.log(decimalToBinary(3.14)); // Expected output: "11"
-
-// Test case 6: Decimal number with repeating fractional part
-console.log(decimalToBinary(1 / 3)); // Expected output: "0.01010101010101010101010101010101"
+describe("decimalToBinary", () => {
+  it("should convert decimal to binary", () => {
+    expect(decimalToBinary(233)).toBe("11101001");
+    expect(decimalToBinary(10)).toBe("1010");
+    expect(decimalToBinary(1000)).toBe("1111101000");
+    expect(Math.floor(5%2)).toBe(1);
+    expect(Math.floor(5/2)).toBe(2);
+    expect(Math.floor(2%2)).toBe(0);
+    expect(Math.floor(2/2)).toBe(1);
+    expect(Math.floor(1%2)).toBe(1);
+    expect(Math.floor(1/2)).toBe(0);
+  });
+});
