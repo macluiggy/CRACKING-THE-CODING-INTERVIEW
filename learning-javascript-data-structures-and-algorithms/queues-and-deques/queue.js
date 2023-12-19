062c39cd59a1ff29d0399dbaf38a6726d0771479
macluiggy <luiggy.macias@imagineapps.co>
2023-12-18 20:05:42 -0500

    Refactor code to improve performance and readability

---
+++ learning-javascript-data-structures-and-algorithms/queues-and-deques/queue.js
@@ -0,0 +1,59 @@
+class Queue {
+  constructor() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+
+  enqueue(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+  dequeue() {
+    if (this.isEmpty) {
+      return undefined;
+    }
+    const result = this.items[this.lowestCount];
+    delete this.items[this.lowestCount];
+    this.lowestCount++;
+    return result;
+  }
+  isEmpty() {
+    return this.size() === 0;
+  }
+  peek() {
+    return this.isEmpty() ? undefined : this.items[this.lowestCount];
+  }
+  size() {
+    // return Object.keys(this.items).length
+    return this.count - this.lowestCount;
+  }
+  clear() {
+    this.items = {};
+    this.count = 0;
+    this.lowestCount = 0;
+  }
+  toString() {
+    if (this.isEmpty()) {
+      return "";
+    }
+    let objString = `${this.items[this.lowestCount]}`;
+    for (let i = this.lowestCount + 1; i < this.count; i++) {
+      objString = `${objString},${this.items[i]}`;
+    }
+    return objString;
+  }
+}
+
+const queue = new Queue();
+console.log(queue.isEmpty()); // outputs true
+queue.enqueue("John");
+queue.enqueue("Jack");
+console.log(queue.toString()); // John,Jack
+queue.enqueue("Camila");
+console.log(queue.toString()); // John,Jack,Camila
+console.log(queue.size()); // outputs 3
+console.log(queue.isEmpty()); // outputs false
+queue.dequeue(); // remove John
+queue.dequeue(); // remove Jack
+console.log(queue.toString()); // Camila
