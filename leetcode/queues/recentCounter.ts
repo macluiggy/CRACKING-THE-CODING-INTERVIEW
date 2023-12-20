589e69f05afd77dd27cee6c9d2d0843f47d417a1
macluiggy <luiggy.macias@imagineapps.co>
2023-12-19 22:18:25 -0500

    Refactor RecentCounter class to extend Queue and implement ping method

--- leetcode/queues/recentCounter.ts
+++ leetcode/queues/recentCounter.ts
@@ -1,11 +1,22 @@
-class RecentCounter {
+import Queue from "./queue";
+
+class RecentCounter extends Queue {
   constructor() {
-      
+    super();
   }
 
   ping(t: number): number {
-      return 2
+    this.enqueue(t);
+    const range = [t - 3000, t];
+    const [x0, xf] = range;
+    const first = this.peek();
+
+    const isFirstInRange = first >= x0 && first <= xf;
+    if (!isFirstInRange) {
+      this.dequeue()
+    }
+    return this.size()
   }
 }
 
-export default RecentCounter
+export default RecentCounter;
