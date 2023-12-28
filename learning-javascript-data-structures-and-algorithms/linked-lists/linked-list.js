24bf2dd432895d0bc2556a80d3629f2d3674a818
macluiggy <luiggy.macias@imagineapps.co>
2023-12-27 20:40:16 -0500

    Add remove() method to LinkedList class

--- learning-javascript-data-structures-and-algorithms/linked-lists/linked-list.js
+++ learning-javascript-data-structures-and-algorithms/linked-lists/linked-list.js
@@ -96,6 +96,12 @@ export default class LinkedList {
     }
     return -1;
   }
+  
+  remove(element) {
+    const index = this.indexOf(element)
+    const removed = this.removeAt(index)
+    return removed
+  }
 }
 
 const list = new LinkedList();
