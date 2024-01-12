c309ea8b3fe321c32719de3972839ac41932acf4
macluiggy <luiggy.macias@imagineapps.co>
2024-01-12 15:05:19 -0500

    Add hasKey, set, and remove methods to Dictionary class

--- learning-javascript-data-structures-and-algorithms/dictionaries-and-hashes/Dictionary.js
+++ learning-javascript-data-structures-and-algorithms/dictionaries-and-hashes/Dictionary.js
@@ -1,7 +1,33 @@
 import { defaultToString } from "../util";
+class ValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+  toString() {
+    return `[#${this.key}: ${this.value}]`;
+  }
+}
 export default class Dictionary {
   constructor(toStrFn = defaultToString) {
     this.toStrFn = toStrFn; // {1}
     this.table = {}; // {2}
+  }
+
+  hasKey(key) {
+    return this.table[this.toStrFn(key)] != null;
+  }
+
+  set(key, value) {
+    if (key == null || value == null) return false;
+    const tableKey = this.toStrFn(key);
+    this.table[tableKey] = new ValuePair(key, value);
+    return true;
+  }
+
+  remove(key) {
+    if (!this.hasKey(key)) return false;
+    delete this.table[this.toStrFn(key)];
+    return true;
   }
 }
