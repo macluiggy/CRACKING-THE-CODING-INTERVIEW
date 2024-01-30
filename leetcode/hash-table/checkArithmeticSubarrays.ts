8ac1bc8b71cc0d7f07439c457d95f55ba9f4a1b5
macluiggy <luiggy.macias@imagineapps.co>
2024-01-30 11:01:43 -0500

    Refactor checkArithmeticSubarrays function

--- leetcode/hash-table/checkArithmeticSubarrays.ts
+++ leetcode/hash-table/checkArithmeticSubarrays.ts
@@ -1,10 +1,34 @@
-function checkArithmeticSubarrays(nums: number[], l: number[], r: number[]): boolean[] {
+function checkArithmeticSubarrays(
+  nums: number[],
+  l: number[],
+  r: number[]
+): boolean[] {
   const subArrays = [];
   for (let i = 0; i < l.length; i++) {
     const subArray = nums.slice(l[i], r[i] + 1);
-    subArrays.push(subArray);
+    subArrays.push(subArray.sort((a, b) => a - b));
   }
-  
-};
+  // console.log(subArrays);
 
-export default checkArithmeticSubarrays
+  let result = [];
+  for (let i = 0; i < subArrays.length; i++) {
+    const subArray = subArrays[i];
+    let isArithmeticDefault = true;
+    for (let j = 0; j < subArray.length; j++) {
+      const e = subArray[j];
+      let isArithmetic =
+        subArray[j + 1] - subArray[j] === subArray[1] - subArray[0];
+        console.log(isArithmetic, j);
+        
+      if (!isArithmetic) {
+        isArithmeticDefault = isArithmetic;
+        break;
+      }
+    }
+    result.push(isArithmeticDefault);
+  }
+  console.log(result);
+  return result
+}
+
+export default checkArithmeticSubarrays;
