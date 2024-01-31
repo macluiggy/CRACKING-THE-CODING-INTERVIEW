f38c29653352f83f8f81b8746151cdc83eef44d1
macluiggy <luiggy.macias@imagineapps.co>
2024-01-31 14:31:12 -0500

    Update Graph class to use Map instead of Dictionary

--- learning-javascript-data-structures-and-algorithms/graphs/Graph.js
+++ learning-javascript-data-structures-and-algorithms/graphs/Graph.js
@@ -1,10 +1,10 @@
-import Dictionary from "../dictionaries-and-hashes/Dictionary";
+// import Dictionary from "../dictionaries-and-hashes/Dictionary.js";
 
 class Graph {
   constructor(isDirected = false) {
     this.isDirected = isDirected; // {1}
     this.vertices = []; // {2}
-    this.adjList = new Dictionary(); // {3}
+    this.adjList = new Map(); // {3}
   }
   addVertex(v) {
     if (!this.vertices.includes(v)) {
@@ -20,6 +20,7 @@ class Graph {
     if (!this.adjList.get(w)) {
       this.addVertex(w); // {9}
     }
+    // console.log(this.adjList.get(v));
     this.adjList.get(v).push(w); // {10}
     if (!this.isDirected) {
       this.adjList.get(w).push(v); // {11}
@@ -32,3 +33,17 @@ class Graph {
     return this.adjList;
   }
 }
+const graph = new Graph();
+const myVertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']; // {12}
+for (let i = 0; i < myVertices.length; i++) { // {13}
+graph.addVertex(myVertices[i]);
+}
+graph.addEdge('A', 'B'); // {14}
+graph.addEdge('A', 'C');
+graph.addEdge('A', 'D');
+graph.addEdge('C', 'D');
+graph.addEdge('C', 'G');
+graph.addEdge('D', 'G');
+graph.addEdge('D', 'H');
+graph.addEdge('B', 'E');
+graph.addEdge('B', 'F');
