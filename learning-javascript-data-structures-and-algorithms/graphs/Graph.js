7b05d5f2ca5466a51c80721eaa89a2076ed48f3a
macluiggy <luiggy.macias@imagineapps.co>
2024-01-31 17:01:21 -0500

    Add shortest path calculation using BFS algorithm

--- learning-javascript-data-structures-and-algorithms/graphs/Graph.js
+++ learning-javascript-data-structures-and-algorithms/graphs/Graph.js
@@ -1,5 +1,6 @@
 // import Dictionary from "../dictionaries-and-hashes/Dictionary.js";
 
+import { Stack } from "../stacks/stack";
 import { BFS, breadthFirstSearch } from "../util";
 
 class Graph {
@@ -69,4 +70,22 @@ graph.addEdge("E", "I");
 // const printVertex = (value) => console.log('Visited vertex: ' + value); // {15}
 // breadthFirstSearch(graph, myVertices[0], printVertex);
 const shortestPathA = BFS(graph, myVertices[0]);
-console.log(shortestPathA)
+console.log(shortestPathA);
+
+const fromVertex = myVertices[0]; // {9}
+for (let i = 1; i < myVertices.length; i++) {
+  // {10}
+  const toVertex = myVertices[i]; // {11}
+  const path = new Stack(); // {12}
+  for (let v = toVertex; v !== fromVertex; v = shortestPathA.predecessors[v]) {
+    // {13}
+    path.push(v); // {14}
+  }
+  path.push(fromVertex); // {15}
+  let s = path.pop(); // {16}
+  while (!path.isEmpty()) {
+    // {17}
+    s += " - " + path.pop(); // {18}
+  }
+  console.log(s); // {19}
+}
