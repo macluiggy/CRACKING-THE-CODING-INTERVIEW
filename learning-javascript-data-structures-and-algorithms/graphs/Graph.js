d618ac78c5bff23ca21b4b484c84ad1e0127bd94
macluiggy <luiggy.macias@imagineapps.co>
2024-01-31 14:26:51 -0500

    Add addEdge, getVertices, and getAdjList methods to Graph class

--- learning-javascript-data-structures-and-algorithms/graphs/Graph.js
+++ learning-javascript-data-structures-and-algorithms/graphs/Graph.js
@@ -13,4 +13,22 @@ class Graph {
       this.adjList.set(v, []); // {7}
     }
   }
+  addEdge(v, w) {
+    if (!this.adjList.get(v)) {
+      this.addVertex(v); // {8}
+    }
+    if (!this.adjList.get(w)) {
+      this.addVertex(w); // {9}
+    }
+    this.adjList.get(v).push(w); // {10}
+    if (!this.isDirected) {
+      this.adjList.get(w).push(v); // {11}
+    }
+  }
+  getVertices() {
+    return this.vertices;
+  }
+  getAdjList() {
+    return this.adjList;
+  }
 }
