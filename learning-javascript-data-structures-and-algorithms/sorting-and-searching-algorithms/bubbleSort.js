3064bd4e84d13c2abdf1782a66bfa57c40f32819
macluiggy <luiggy.macias@imagineapps.co>
2024-02-01 13:42:08 -0500

    Update function to handle edge cases

---
+++ learning-javascript-data-structures-and-algorithms/sorting-and-searching-algorithms/bubbleSort.js
@@ -0,0 +1,18 @@
+import { Compare, defaultCompare, swap } from "../util";
+
+function bubbleSort(array, compareFn = defaultCompare) {
+  const { length } = array; // {1}
+  for (let i = 0; i < length; i++) {
+    // {2}
+    for (let j = 0; j < length - 1; j++) {
+      // {3}
+      if (compareFn(array[j], array[j + 1]) === Compare.BIGGER_THAN) {
+        // {4}
+        swap(array, j, j + 1); // {5}
+      }
+    }
+  }
+  return array;
+}
+
+export default bubbleSort
---
+++ learning-javascript-data-structures-and-algorithms/sorting-and-searching-algorithms/bubbleSort.test.js
@@ -0,0 +1,29 @@
+import bubbleSort from "./bubbleSort";
+
+describe("bubbleSort", () => {
+  it("should sort the array in ascending order", () => {
+    const array = [5, 3, 8, 4, 2];
+    const sortedArray = bubbleSort(array);
+    expect(sortedArray).toEqual([2, 3, 4, 5, 8]);
+  });
+
+  it("should handle empty array", () => {
+    const array = [];
+    const sortedArray = bubbleSort(array);
+    expect(sortedArray).toEqual([]);
+  });
+
+  it("should handle array with one element", () => {
+    const array = [5];
+    const sortedArray = bubbleSort(array);
+    expect(sortedArray).toEqual([5]);
+  });
+
+  it("should handle array with duplicate elements", () => {
+    const array = [5, 3, 8, 4, 2, 8, 2];
+    const sortedArray = bubbleSort(array);
+    expect(sortedArray).toEqual([2, 2, 3, 4, 5, 8, 8]);
+  });
+});
+
+console.log(bubbleSort([5, 3, 8, 4, 2])); // Output: [2, 3, 4, 5, 8]
