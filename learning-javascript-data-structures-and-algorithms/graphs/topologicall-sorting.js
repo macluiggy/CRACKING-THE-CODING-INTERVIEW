0682ab288623bbae36ceafd64f07e109007067de
macluiggy <luiggy.macias@imagineapps.co>
2024-02-01 11:37:39 -0500

    Add topological sorting algorithm implementation

--- learning-javascript-data-structures-and-algorithms/graphs/topologicall-sorting.js
+++ learning-javascript-data-structures-and-algorithms/graphs/topologicall-sorting.js
@@ -13,3 +13,18 @@ graph.addEdge("B", "E");
 graph.addEdge("C", "F");
 graph.addEdge("F", "E");
 const result = DFS(graph);
+const fTimes = result.finished;
+let s = "";
+for (let count = 0; count < myVertices.length; count++) {
+  let max = 0;
+  let maxName = null;
+  for (let i = 0; i < myVertices.length; i++) {
+    if (fTimes[myVertices[i]] > max) {
+      max = fTimes[myVertices[i]];
+      maxName = myVertices[i];
+    }
+  }
+  s += " - " + maxName;
+  delete fTimes[maxName];
+}
+console.log(s);
