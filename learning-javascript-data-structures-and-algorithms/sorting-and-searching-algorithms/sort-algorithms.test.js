18a686b439dd5445c5d92abac039fc2d781730a4
macluiggy <luiggy.macias@imagineapps.co>
2024-02-02 10:21:23 -0500

    Refactor sort-algorithms.test.js

--- learning-javascript-data-structures-and-algorithms/sorting-and-searching-algorithms/sort-algorithms.test.js
+++ learning-javascript-data-structures-and-algorithms/sorting-and-searching-algorithms/sort-algorithms.test.js
@@ -13,23 +13,23 @@ describe("bubbleSort", () => {
       expect(sortedArray).toEqual([2, 3, 4, 5, 8]);
     });
 
-    // it("should handle empty array", () => {
-    //   const array = [];
-    //   const sortedArray = sortFunction(array);
-    //   expect(sortedArray).toEqual([]);
-    // });
+    it("should handle empty array", () => {
+      const array = [];
+      const sortedArray = sortFunction(array);
+      expect(sortedArray).toEqual([]);
+    });
 
-    // it("should handle array with one element", () => {
-    //   const array = [5];
-    //   const sortedArray = sortFunction(array);
-    //   expect(sortedArray).toEqual([5]);
-    // });
+    it("should handle array with one element", () => {
+      const array = [5];
+      const sortedArray = sortFunction(array);
+      expect(sortedArray).toEqual([5]);
+    });
 
-    // it("should handle array with duplicate elements", () => {
-    //   const array = [5, 3, 8, 4, 2, 8, 2];
-    //   const sortedArray = sortFunction(array);
-    //   expect(sortedArray).toEqual([2, 2, 3, 4, 5, 8, 8]);
-    // });
+    it("should handle array with duplicate elements", () => {
+      const array = [5, 3, 8, 4, 2, 8, 2];
+      const sortedArray = sortFunction(array);
+      expect(sortedArray).toEqual([2, 2, 3, 4, 5, 8, 8]);
+    });
   }
 });
 
