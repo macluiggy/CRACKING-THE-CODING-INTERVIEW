f492a8c2914d96d7dc67228b1ee8dbc53a5e2d87
macluiggy <luiggy.macias@imagineapps.co>
2024-02-05 10:47:22 -0500

    Add binarySearchRecursive function

---
+++ learning-javascript-data-structures-and-algorithms/algorithm-designs-and-techniques/binarySearchRecursive.js
@@ -0,0 +1,31 @@
+import quickSort from "../sorting-and-searching-algorithms/quickSort";
+import { Compare, defaultCompare } from "../util";
+
+function binarySearchRecursive(
+  array,
+  value,
+  low,
+  high,
+  compareFn = defaultCompare
+) {
+  if (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    const element = array[mid];
+    if (compareFn(element, value) === Compare.LESS_THAN) {
+      // {1}
+      return binarySearchRecursive(array, value, mid + 1, high, compareFn);
+    } else if (compareFn(element, value) === Compare.BIGGER_THAN) {
+      // {2}
+      return binarySearchRecursive(array, value, low, mid - 1, compareFn);
+    } else {
+      return mid; // {3}
+    }
+  }
+  return DOES_NOT_EXIST; // {4}
+}
+export function binarySearch(array, value, compareFn = defaultCompare) {
+  const sortedArray = quickSort(array);
+  const low = 0;
+  const high = sortedArray.length - 1;
+  return binarySearchRecursive(array, value, low, high, compareFn);
+}
