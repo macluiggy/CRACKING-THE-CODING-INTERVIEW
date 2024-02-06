2b6feb0872385b7f8dc0688dfe4e96ad14e5e3c8
macluiggy <luiggy.macias@imagineapps.co>
2024-02-06 12:03:50 -0500

    Add binary search algorithm to searchInsert function

--- leetcode/searchInsert.ts
+++ leetcode/searchInsert.ts
@@ -1,3 +1,19 @@
-function searchInsert(nums: number[], target: number): number {}
+function searchInsert(nums: number[], target: number): number {
+  let low = 0;
+  let high = nums.length - 1;
+  while (low <= high) {
+    const middle = Math.floor((low + high) / 2);
+    console.log(middle);
+    
+    const middleElement = nums[middle];
+    if (middleElement < target) {
+      low = middle + 1;
+    } else if (middleElement > target) {
+      high = middle - 1;
+    } else {
+      return middle;
+    }
+  }
+}
 
 export default searchInsert;
