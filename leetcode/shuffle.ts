d3cca621eff3a9f4fec7832bfeb5bfdf3f9bfeb9
macluiggy <luiggy.macias@imagineapps.co>
2024-02-08 09:45:13 -0500

    Add shuffle function to leetcode/shuffle.ts

---
+++ leetcode/shuffle.ts
@@ -0,0 +1,16 @@
+function shuffle(nums: number[], n: number): number[] {
+  const firstHalf = nums.slice(0, n);
+  const secondHalf = nums.slice(n);
+  console.log(firstHalf, secondHalf);
+  
+  // Rest of the code...
+  let result = []
+  for (let i = 0; i < firstHalf.length; i++) {
+    const f = firstHalf[i];
+    const s =  secondHalf[i]
+    result.push(f, s)
+  }
+  console.log(result);
+  return result
+}
+console.log(shuffle([2,5,1,3,4,7],3));
