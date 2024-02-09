8080013cd3b5620554d868775267441885bdc13b
macluiggy <luiggy.macias@imagineapps.co>
2024-02-08 19:35:05 -0500

    Refactor maximumWealth function to use arrow functions and improve code readability

--- leetcode/maximumWealth.ts
+++ leetcode/maximumWealth.ts
@@ -1,10 +1,14 @@
 function maximumWealth(accounts: number[][]): number {
-    const a = accounts.map((moneyInBank: number[]) => {
-      const sum = moneyInBank.reduce((prev, current) => current + prev, 0)
-      return sum
-    })
-    let max = Math.max(...a)
-    return max
-};
+  return Math.max(
+    ...accounts.map((moneyInBank: number[]) =>
+      moneyInBank.reduce((prev, current) => current + prev, 0)
+    )
+  );
+}
 
-console.log(maximumWealth([[1,2,3],[3,2,1]]));
+console.log(
+  maximumWealth([
+    [1, 2, 3],
+    [3, 2, 1],
+  ])
+);
