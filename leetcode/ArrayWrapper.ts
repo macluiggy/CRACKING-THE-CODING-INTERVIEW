b86b27d5e7222f72f78eab189ce84bec29cca698
macluiggy <luiggy.macias@imagineapps.co>
2024-02-08 19:48:12 -0500

    Add ArrayWrapper class for wrapping arrays

---
+++ leetcode/ArrayWrapper.ts
@@ -0,0 +1,26 @@
+class ArrayWrapper {
+    
+  constructor(nums: number[]) {
+      return 10
+  }
+  
+  valueOf(): number {
+      
+  }
+  
+  toString(): string {
+      
+  }
+};
+
+/**
+* const obj1 = new ArrayWrapper([1,2]);
+* const obj2 = new ArrayWrapper([3,4]);
+* obj1 + obj2; // 10
+* String(obj1); // "[1,2]"
+* String(obj2); // "[3,4]"
+*/
+
+const obj1 = new ArrayWrapper([1,2]);
+const obj2 = new ArrayWrapper([3,4]);
+obj1 + obj2; // 10
