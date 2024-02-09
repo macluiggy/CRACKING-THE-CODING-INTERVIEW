3cde113e1d8105b910b47173c82645e0a51a9a24
macluiggy <luiggy.macias@imagineapps.co>
2024-02-08 21:00:59 -0500

    Add removeDuplicates method to LinkedList class

---
+++ chapter-2-linked-lists/remove-dups.ts
@@ -0,0 +1,35 @@
+class ListNode {
+  value: number;
+  next: ListNode | null;
+
+  constructor(value: number, next: ListNode | null = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  head: ListNode | null;
+
+  constructor() {
+    this.head = null;
+  }
+
+  // Method to add a node to the end of the linked list
+  add(value: number): void {
+    if (!this.head) {
+      this.head = new ListNode(value);
+    } else {
+      let current: ListNode | null = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = new ListNode(value);
+    }
+  }
+
+  // Method to remove duplicates from the linked list
+  removeDuplicates(): void {
+    // Your code goes here
+  }
+}
