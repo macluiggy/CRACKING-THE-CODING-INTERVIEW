e3ccf446ac53fa71878ed3ab281f2d1deb17170a
macluiggy <luiggy.macias@imagineapps.co>
2024-02-08 19:20:06 -0500

    Fix calculation bug in differenceOfSums function

--- leetcode/differenceOfSums.ts
+++ leetcode/differenceOfSums.ts
@@ -1,19 +1,19 @@
 function differenceOfSums(n: number, m: number): number {
   let num1 = 0;
-  let arr1 = []
+  let arr1 = [];
   let num2 = 0;
-  let arr2 = []
+  let arr2 = [];
   for (let i = 1; i <= n; i++) {
-    if (m % i !== 0) {
+    if (i % m !== 0) {
       num1 += i;
-      arr1.push(i)
+      arr1.push(i);
     } else {
       num2 += i;
-      arr2.push(i)
+      arr2.push(i);
     }
   }
   console.log(arr1, arr2);
-  
+
   let result = num1 - num2;
   return result;
 }
