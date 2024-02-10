eaf80095632c95152699d90acd2db91f9003ba8b
macluiggy <luiggy.macias@imagineapps.co>
2024-02-09 23:43:39 -0500

    Add canFinish function to check if courses can be finished

---
+++ leetcode/70-blind-leetcode/graphs/canFinish.ts
@@ -0,0 +1,33 @@
+function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+  const preMap = new Map();
+  for (let i = 0; i < numCourses; i++) {
+    preMap.set(i, []);
+  }
+  for (let i = 0; i < prerequisites.length; i++) {
+    const [crs, pre] = prerequisites[i];
+    if (preMap.get(crs)) {
+      preMap.get(crs).push(pre);
+    }
+  }
+
+  const visitedSet = new Set();
+
+  const dfs = (crs: any) => {
+    if (visitedSet.has(crs)) return false;
+    if (preMap.get(crs).length === 0) return true;
+
+    visitedSet.add(crs);
+
+    for (let pre of preMap.get(crs)) {
+      if (!dfs(pre)) return false;
+    }
+    visitedSet.delete(crs);
+    preMap.set(crs, []);
+    return true;
+  };
+
+  for (let crs = 0; crs < numCourses; crs++) {
+    if (!dfs(crs)) return false;
+  }
+  return true
+}
