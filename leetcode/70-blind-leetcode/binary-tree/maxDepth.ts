5a32d9fa9ebf2f1ff3abeb40c3726869b62511ab
macluiggy <luiggy.macias@imagineapps.co>
2024-02-10 23:40:23 -0500

    Add iterative solution for maxDepth function

--- leetcode/70-blind-leetcode/binary-tree/maxDepth.ts
+++ leetcode/70-blind-leetcode/binary-tree/maxDepth.ts
@@ -15,6 +15,28 @@
 import TreeNode from "./TreeNode";
 
 function maxDepth(root: TreeNode | null): number {
-    if (!root) return 0
-    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
-};
+  if (!root) return 0;
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+}
+
+function maxDepthIterative(root: TreeNode | null): number {
+  if (!root) return 0;
+  const stack = [[root, 1]];
+  let res = 1;
+  while (stack.length) {
+    const pop = stack.pop()!;
+    const node = pop[0] as TreeNode; // Ensure node is of type TreeNode
+    let deep = pop[1];
+    if (node) {
+      res = Math.max(res, deep as number);
+      if (node.left !== null) {
+        stack.push([node.left, deep as number + 1]);
+      }
+      if (node.right !== null) {
+        stack.push([node.right, deep as number + 1]);
+      }
+    }
+  }
+
+  return res;
+}
