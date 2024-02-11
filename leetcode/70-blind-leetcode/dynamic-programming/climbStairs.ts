5a1d215c28fc20b1a6028663f89fff81b2b63650
macluiggy <luiggy.macias@imagineapps.co>
2024-02-11 13:36:53 -0500

    Implement dynamic programming solution for climbing stairs

--- leetcode/70-blind-leetcode/dynamic-programming/climbStairs.ts
+++ leetcode/70-blind-leetcode/dynamic-programming/climbStairs.ts
@@ -1,1 +1,10 @@
-function climbStairs(n: number): number {}
+function climbStairs(n: number): number {
+  let [one, two] = [1, 1];
+
+  for (let i = 0; i < n; i++) {
+    let temp = one;
+    one = one + two;
+    two = temp;
+  }
+  return one;
+}
