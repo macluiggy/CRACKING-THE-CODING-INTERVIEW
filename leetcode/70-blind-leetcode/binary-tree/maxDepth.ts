b4b9abba82ed044b0eb0dde1a1e0f8befe36a8d7
macluiggy <luiggy.macias@imagineapps.co>
2024-02-10 20:12:26 -0500

    Add TreeNode class and implement maxDepth function

--- leetcode/70-blind-leetcode/binary-tree/maxDepth.ts
+++ leetcode/70-blind-leetcode/binary-tree/maxDepth.ts
@@ -12,6 +12,9 @@
  * }
  */
 
+import TreeNode from "./TreeNode";
+
 function maxDepth(root: TreeNode | null): number {
-    
+    if (!root) return 0
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
 };
