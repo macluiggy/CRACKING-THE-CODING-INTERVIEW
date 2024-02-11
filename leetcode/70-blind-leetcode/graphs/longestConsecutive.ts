3556aa8dad5eb19911ad18700e6634c80bc4d13b
macluiggy <luiggy.macias@imagineapps.co>
2024-02-10 19:54:56 -0500

    Refactor longestConsecutive function to improve performance

--- leetcode/70-blind-leetcode/graphs/longestConsecutive.ts
+++ leetcode/70-blind-leetcode/graphs/longestConsecutive.ts
@@ -1,3 +1,18 @@
 function longestConsecutive(nums: number[]): number {
-    
-};
+  const numsSet = new Set(nums);
+  let longestStreak = 0;
+
+  for (let num of numsSet) {
+    if (!numsSet.has(num - 1)) { // check if current number is the start of a sequence
+      let length = 0
+
+      while (numsSet.has(length + 1)) { // check if next number in sequence exists
+        length++
+      }
+
+      longestStreak = Math.max(longestStreak, length);
+    }
+  }
+
+  return longestStreak;
+}
