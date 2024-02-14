9bd546fda3e105a19c8559ccb2b6fd43e98962cd
macluiggy <luiggy.macias@imagineapps.co>
2024-02-14 12:57:55 -0500

    Refactor lengthOfLIS function to improve performance

--- leetcode/70-blind-leetcode/dynamic-programming/lengthOfLIS.ts
+++ leetcode/70-blind-leetcode/dynamic-programming/lengthOfLIS.ts
@@ -1,16 +1,29 @@
+// function lengthOfLIS(nums: number[]): number {
+//   if (nums.length == 0) return 0;
+//   let dp: number[] = new Array(nums.length).fill(1);
+//   let max = 1;
+//   for (let i = 1; i < nums.length; i++) {
+//     for (let j = 0; j < i; j++) {
+//       if (nums[i] > nums[j]) {
+//         dp[i] = Math.max(dp[i], dp[j] + 1);
+//       }
+//     }
+//     max = Math.max(max, dp[i]);
+//   }
+//   return max;
+// }
+
 function lengthOfLIS(nums: number[]): number {
   if (nums.length == 0) return 0;
   let dp: number[] = new Array(nums.length).fill(1);
-  let max = 1;
-  for (let i = 1; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[i] > nums[j]) {
+  for (let i = nums.length - 1; i >= 0; i--) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] < nums[j]) {
         dp[i] = Math.max(dp[i], dp[j] + 1);
       }
     }
-    max = Math.max(max, dp[i]);
   }
-  return max;
+  return Math.max(...dp);
 }
 
-console.log(lengthOfLIS([10,9,2,5,3,7,101,18]));  // Output: 4
+console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])); // Output: 4
