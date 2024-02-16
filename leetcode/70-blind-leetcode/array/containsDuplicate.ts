f3545333c5830f5e9bf1571da8395e457f7ba49a
macluiggy <luiggy.macias@imagineapps.co>
2024-02-16 17:39:51 -0500

    Refactor containsDuplicate function to use a map for efficient duplicate checking

--- leetcode/70-blind-leetcode/array/containsDuplicate.ts
+++ leetcode/70-blind-leetcode/array/containsDuplicate.ts
@@ -1,3 +1,15 @@
 function containsDuplicate(nums: number[]): boolean {
-    
-};
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (map.get(num)) {
+      return !false;
+    }
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+
+  return !true;
+}
+
+console.log(containsDuplicate([1, 2, 3, 1]));
