aee062f5401a98c998802c4007a88b518d6d19f0
macluiggy <luiggy.macias@imagineapps.co>
2024-02-16 10:52:47 -0500

    Refactor topKFrequent function to use a map for counting and return the top k frequent numbers

--- leetcode/70-blind-leetcode/heap/topKFrequent.ts
+++ leetcode/70-blind-leetcode/heap/topKFrequent.ts
@@ -1,3 +1,24 @@
 function topKFrequent(nums: number[], k: number): number[] {
-    
-};
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+  let ans = [];
+  for (let i = 0; i < k; i++) {
+    let max = 0;
+    let maxKey = 0;
+    for (let [key, value] of map.entries()) {
+      max = Math.max(max, value);
+      if (max == value) {
+        maxKey = key;
+      }
+    }
+    map.delete(maxKey);
+    ans.push(maxKey);
+  }
+  // console.log(ans);
+  return ans;
+}
+
+console.log(topKFrequent([1, 1, 1, 2, 2, 3], 2));
