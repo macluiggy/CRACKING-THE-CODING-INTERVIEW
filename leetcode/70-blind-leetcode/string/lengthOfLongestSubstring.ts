361172a5801fbf66dcb94f2768a81a934bdb90f3
macluiggy <luiggy.macias@imagineapps.co>
2024-02-19 09:38:45 -0500

    Refactor lengthOfLongestSubstring function

--- leetcode/70-blind-leetcode/string/lengthOfLongestSubstring.ts
+++ leetcode/70-blind-leetcode/string/lengthOfLongestSubstring.ts
@@ -1,3 +1,37 @@
+// function lengthOfLongestSubstring(s: string): number {
+//   let str = "";
+//   let arrString = [];
+//   for (let i = 0; i < s.length; i++) {
+//     const char = s[i];
+//     let strAux = str + char;
+//     if (strAux.length === new Set(strAux).size) {
+//       str += char;
+//     } else {
+//       arrString.push(str);
+//       str = char;
+//     }
+//   }
+//   console.log(arrString);
+//   const arrLengthString = arrString.map((s) => s.length);
+//   return Math.max(...arrLengthString);
+// }
 function lengthOfLongestSubstring(s: string): number {
-    
-};
+  let longestLength = 0;
+  let substrStart = 0;
+  const charIndexMap = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    if (charIndexMap.has(char)) {
+      substrStart = Math.max(substrStart, charIndexMap.get(char) + 1);
+    }
+
+    charIndexMap.set(char, i);
+    longestLength = Math.max(longestLength, i - substrStart + 1);
+  }
+
+  return longestLength;
+}
+
+console.log(lengthOfLongestSubstring("abcabcdbb")); // Output: 3
+console.log(lengthOfLongestSubstring("abcabcbb"));
