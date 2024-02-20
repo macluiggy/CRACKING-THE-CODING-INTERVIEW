72aa3ec44ab7d1e74215b044ea076ed03115b692
macluiggy <luiggy.macias@imagineapps.co>
2024-02-20 10:57:46 -0500

    Refactor productExceptSelf function to calculate left and right products

--- leetcode/70-blind-leetcode/array/productExceptSelf.ts
+++ leetcode/70-blind-leetcode/array/productExceptSelf.ts
@@ -1,3 +1,22 @@
-function productExceptSelf(nums: number[]): number[] {
-    
-};
+function productExceptSelf(nums: number[]) {
+  let n = nums.length;
+  let output = new Array(n).fill(1);
+
+  let leftProduct = 1;
+  for (let i = 0; i < n; i++) {
+    output[i] *= leftProduct;
+    leftProduct *= nums[i];
+  }
+  console.log(output);
+  
+
+  let rightProduct = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    output[i] *= rightProduct;
+    rightProduct *= nums[i];
+  }
+
+  return output;
+}
+
+console.log(productExceptSelf([1, 2, 3, 4]));  // Output: [24, 12, 8, 6]
