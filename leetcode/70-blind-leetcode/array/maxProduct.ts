cb9fb35ba61cb326c323fe1a1f4437168d71d3b3
macluiggy <luiggy.macias@imagineapps.co>
2024-02-22 11:26:19 -0500

    Refactor maxProduct function to improve performance and readability

--- leetcode/70-blind-leetcode/array/maxProduct.ts
+++ leetcode/70-blind-leetcode/array/maxProduct.ts
@@ -1,26 +1,43 @@
+// function maxProduct(nums: number[]): number {
+//   let r = productOfNegatives(nums);
+//   if (r) {
+//     return r;
+//   }
+//   let prod = nums[0];
+//   let maxProd = prod;
+
+//   for (let i = 1; i < nums.length; i++) {
+//     prod = Math.max(prod * nums[i], nums[i]);
+//     maxProd = Math.max(prod, maxProd);
+//   }
+
+//   return maxProd;
+// }
+
+// function productOfNegatives(nums: number[]): number | null {
+//   let negatives = nums.filter((num) => num < 0);
+//   if (negatives.length % 2 === 0) {
+//     return negatives.reduce((prod, num) => prod * num, 1);
+//   }
+//   return null;
+// }
 function maxProduct(nums: number[]): number {
-  let r = productOfNegatives(nums);
-  if (r) {
-    return r;
-  }
-  let prod = nums[0];
-  let maxProd = prod;
+  let maxProd = nums[0];
+  let minProd = nums[0];
+  let result = maxProd;
 
-  for (let i = 0; i < nums.length; i++) {
-    prod = Math.max(prod * nums[i], nums[i]);
-    maxProd = Math.max(prod, maxProd);
-    console.log(maxProd);
+  for (let i = 1; i < nums.length; i++) {
+    let tempMax = maxProd;
+    maxProd = Math.max(nums[i], maxProd * nums[i], minProd * nums[i]);
+    minProd = Math.min(nums[i], tempMax * nums[i], minProd * nums[i]);
+    result = Math.max(result, maxProd);
   }
 
-  return maxProd;
+  return result;
 }
-function productOfNegatives(nums: number[]): number | null {
-  let negatives = nums.filter((num) => num < 0);
-  if (negatives.length % 2 === 0) {
-    return nums.reduce((prod, num) => prod * num, 1);
-  }
-  return null;
-}
+
+
+console.log(maxProduct([2, -5, -2, -4, 3]));  // Output: 24
 // console.log(maxProduct([2,3,-2,4]));
 // console.log(maxProduct([-2]));
 // console.log(maxProduct([-2, 0, -1]));
