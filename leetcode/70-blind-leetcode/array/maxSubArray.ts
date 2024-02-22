7b2f35e892f416e69f808969e3d78b0916f87bc7
macluiggy <luiggy.macias@imagineapps.co>
2024-02-21 21:08:07 -0500

    Refactor maxSubArray function to improve performance

--- leetcode/70-blind-leetcode/array/maxSubArray.ts
+++ leetcode/70-blind-leetcode/array/maxSubArray.ts
@@ -30,7 +30,19 @@ function isEveryNumberNegative(nums: number[]) {
 //   }
 //   return maxSum;
 // }
+// function maxSubArray(nums: number[]): number {
+//   let maxSum = -Infinity;
+//   for (let i = 0; i < nums.length; i++) {
+//     let currentSum = 0;
+//     for (let j = i; j < nums.length; j++) {
+//       currentSum += nums[j];
+//       maxSum = Math.max(maxSum, currentSum);
+//     }
+//   }
+//   return maxSum;
+// }
 
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));  // Output: 6
 console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4])); // Output: 6
 console.log(
   maxSubArray([
