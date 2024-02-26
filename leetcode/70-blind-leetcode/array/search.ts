288d3ef03f07ca4d2969893562266b09d71cc62b
macluiggy <luiggy.macias@imagineapps.co>
2024-02-26 12:01:22 -0500

    Refactor search algorithm in search.ts

--- leetcode/70-blind-leetcode/array/search.ts
+++ leetcode/70-blind-leetcode/array/search.ts
@@ -60,23 +60,32 @@ function search(nums: number[], target: number): number {
   let left = 0;
   let right = nums.length - 1;
 
-  // If the array is not rotated, return the first element
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
     if (nums[mid] == target) {
       return mid;
     }
     if (nums[mid] >= nums[left]) {
-      if (target < nums[left] || target > nums[mid]) {
-        left = mid + 1;
-      } else {
+      // if (target < nums[left] || target > nums[mid]) {
+      //   left = mid + 1;
+      // } else {
+      //   right = mid - 1;
+      // }
+      if (target >= nums[left] && target <= nums[mid]) {
         right = mid - 1;
+      } else {
+        left = mid + 1;
       }
     } else {
-      if (target > nums[right] || target < nums[mid]) {
-        right = mid - 1;
-      } else {
+      // if (target > nums[right] || target < nums[mid]) {
+      //   right = mid - 1;
+      // } else {
+      //   left = mid + 1;
+      // }
+      if (target >= nums[mid] && target <= nums[right]) {
         left = mid + 1;
+      } else {
+        right = mid - 1;
       }
     }
   }
