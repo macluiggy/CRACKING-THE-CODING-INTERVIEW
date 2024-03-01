a1b05a62534523675c4e777ebbf83d781a8366d2
macluiggy <luiggy.macias@imagineapps.co>
2024-03-01 12:25:09 -0500

    Refactor missingNumber function to improve performance

--- leetcode/70-blind-leetcode/binary/missingNumber.ts
+++ leetcode/70-blind-leetcode/binary/missingNumber.ts
@@ -1,3 +1,17 @@
+// function missingNumber(nums: number[]): number {
+//   nums.sort((a, b) => a - b);;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] !==i) {
+//       return i
+//     }
+//   }
+//   return nums.length
+// }
 function missingNumber(nums: number[]): number {
-    
-};
+  let res = nums.length;
+  for (let i = 0; i < nums.length; i++) {
+    res += i - nums[i];
+  }
+  return res
+}
+console.log(missingNumber([3, 0, 1]));
