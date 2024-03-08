01fc7558ff2835cdbf32b7ad983c5cdafb2b8e3e
macluiggy <luiggy.macias@imagineapps.co>
2024-03-08 09:18:43 -0500

    Add removeNthFromEnd function to linked-lists

---
+++ leetcode/70-blind-leetcode/linked-lists/removeNthFromEnd.ts
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+import ListNode from "./listNode";
+
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+    
+};
