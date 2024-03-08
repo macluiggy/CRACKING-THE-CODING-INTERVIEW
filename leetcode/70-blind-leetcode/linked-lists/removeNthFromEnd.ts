68acbed53b93fa2b35eb96667fd3efe9d86bdb3b
macluiggy <luiggy.macias@imagineapps.co>
2024-03-08 09:54:05 -0500

    Add removeNthFromEnd function to linked-lists/removeNthFromEnd.ts

--- leetcode/70-blind-leetcode/linked-lists/removeNthFromEnd.ts
+++ leetcode/70-blind-leetcode/linked-lists/removeNthFromEnd.ts
@@ -13,5 +13,33 @@
 import ListNode from "./listNode";
 
 function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
-    
-};
+  let listSize = 0;
+  let curr = head;
+  while (curr) {
+    listSize++;
+    curr = curr.next;
+  }
+
+  if (n > listSize) {
+    return head;
+  }
+  let dummy = new ListNode(head?.val)
+  dummy.next = head
+  let position = listSize - n;
+  curr = dummy;
+  for (let i = 0; i < position; i++) {
+    curr = curr?.next;
+  }
+  curr.next = curr.next.next;
+  return dummy.next
+}
+
+const head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head.next.next.next = new ListNode(4);
+head.next.next.next.next = new ListNode(5);
+
+const n = 2;
+const result = removeNthFromEnd(head, n);
+console.log(result);
