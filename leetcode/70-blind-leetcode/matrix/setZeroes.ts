69f34ae613bbea456643fa00bd0b344ef29bf144
macluiggy <luiggy.macias@imagineapps.co>
2024-03-12 09:18:58 -0500

    Refactor setZeroes function to optimize performance

--- leetcode/70-blind-leetcode/matrix/setZeroes.ts
+++ leetcode/70-blind-leetcode/matrix/setZeroes.ts
@@ -5,29 +5,27 @@ function setZeroes(matrix: number[][]): void {
   let jSet = new Set();
   let iSet = new Set();
   for (let i = 0; i < matrix.length; i++) {
-    const row = matrix[i];
-    for (let j = 0; j < row.length; j++) {
-      const col = row[j];
-      if (col == 0) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
         jSet.add(j);
         iSet.add(i);
       }
     }
   }
-  let matrixCopy = JSON.parse(JSON.stringify(matrix));
   for (let i = 0; i < matrix.length; i++) {
-    const row = matrix[i];
-    let changeAllValuesOfJInI = iSet.has(i);
-    let changeAllValuesOfIinJ = false;
-    for (let j = 0; j < row.length; j++) {
-      if (changeAllValuesOfJInI) {
-        matrix[i][j] = 0;
-      }
+    if (iSet.has(i)) {
+      matrix[i] = new Array(matrix[0].length).fill(0);
+      continue;
+    }
+    for (let j = 0; j < matrix[i].length; j++) {
       if (jSet.has(j)) {
-        matrix[i][j] = 0
+        matrix[i][j] = 0;
       }
     }
   }
-  console.log(matrix);
 }
-setZeroes([[0,1,2,0],[3,4,5,2],[1,3,1,5]]);
+setZeroes([
+  [0, 1, 2, 0],
+  [3, 4, 5, 2],
+  [1, 3, 1, 5],
+]);
