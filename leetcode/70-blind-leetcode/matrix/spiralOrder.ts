c5c2a0d054d588ea42ec1f3f21c5f19cf8e571dc
macluiggy <luiggy.macias@imagineapps.co>
2024-03-13 11:17:43 -0500

    Refactor spiralOrder function to handle matrix traversal

--- leetcode/70-blind-leetcode/matrix/spiralOrder.ts
+++ leetcode/70-blind-leetcode/matrix/spiralOrder.ts
@@ -1,5 +1,48 @@
 function spiralOrder(matrix: number[][]): number[] {
+  let ans = [];
+  let fromUpRight = true;
+  let i = 0;
+  let length = matrix.length;
+  while (matrix.length) {
+    // console.log(matrix);
     
-};
+    if (fromUpRight && i == 0) {
+      ans.push(matrix.shift());
+      // i++;
+      continue;
+    }
+    if (fromUpRight && i !== length) {
+      ans.push(matrix[i].pop());
+      i++
+      continue;
+    }
+    if (i === length) {
+      ans.push(matrix.pop()?.reverse());
+      fromUpRight = false
+      length = matrix.length
+      i--
+      continue
+    }
+    if (!fromUpRight && i !==0) {
+      ans.push(matrix[i].shift())
+      i--
+      continue
+    }
+    if (!fromUpRight && i == 0) {
+      ans.push(matrix.shift())
+      i++
+      continue
+    }
+  }
+  console.log(ans);
+  
+  return ans
+}
 
-console.log(spiralOrder([[1,2,3],[4,5,6],[7,8,9]]));
+console.log(
+  spiralOrder([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
