dabe91f822b0b0aa0fd1d703d651f2e3c7fd4a25
macluiggy <luiggy.macias@imagineapps.co>
2024-03-18 10:43:35 -0500

    Add DFS algorithm for matrix word search

--- leetcode/70-blind-leetcode/matrix/exist.ts
+++ leetcode/70-blind-leetcode/matrix/exist.ts
@@ -1,3 +1,43 @@
 function exist(board: string[][], word: string): boolean {
-    
-};
+  const [ROWS, COLS] = [board.length, board[0].length];
+  let path = new Set();
+
+  const dfs: any = (r, c, i) => {
+    let pathrc = `${r}-${c}`;
+    if (i === word.length) return true;
+    if (
+      r < 0 ||
+      c < 0 ||
+      r > ROWS ||
+      c > COLS ||
+      word[i] !== board[r][c] ||
+      path.has(pathrc)
+    )
+      return false;
+      path.add(pathrc)
+    let res: boolean =
+      dfs(r + 1, c, i + 1) ||
+      dfs(r - 1, c, i + 1) ||
+      dfs(r, c + 1, i + 1) ||
+      dfs(r, c - 1, i + 1);
+    path.delete(pathrc);
+    return res;
+  };
+  for (let r = 0; r < ROWS; r++) {
+    for (let c = 0; c < COLS; c++) {
+      if (dfs(r, c, 0)) return true;
+    }
+  }
+  return false;
+}
+
+console.log(
+  exist(
+    [
+      ["A", "B", "C", "E"],
+      ["S", "F", "C", "S"],
+      ["A", "D", "E", "E"],
+    ],
+    "ABCCED"
+  )
+);
