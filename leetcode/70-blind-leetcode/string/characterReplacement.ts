f8913e54fbceac49d708045539c8aab2217d0445
macluiggy <luiggy.macias@imagineapps.co>
2024-03-19 11:43:31 -0500

    Refactor characterReplacement function to improve performance

--- leetcode/70-blind-leetcode/string/characterReplacement.ts
+++ leetcode/70-blind-leetcode/string/characterReplacement.ts
@@ -1,3 +1,57 @@
-function characterReplacement(s: string, k: number): number {}
+// function characterReplacement(s: string, k: number): number {
+//   let continueWhile = true;
+//   let tempK = k;
+//   let curr = 0;
+//   let map = new Map();
+//   let max = 0;
+//   while (continueWhile) {
+//     let currLetter = s[curr];
+//     let tempS = s;
+//     for (let i = curr; i < curr + tempK; i++) {
+//       console.log(i);
+      
+//       if (currLetter == s[i]) {
+//         continue;
+//       } else {
+//         tempS = tempS.substring(0, i) + currLetter + tempS.substring(i + 1);
+//       }
+//     }
+//     let map = new Map();
+//     for (let i = 0; i < tempS.length; i++) {
+//       const char = tempS[i];
+//       map.set(char, map.get(char) || 0 + 1);
+//     }
+//     for (let char of map) {
+//       max = Math.max(map.get(char), max);
+//     }
+//     curr++;
+//     continueWhile = !(curr == s.length);
+//   }
+//   return max
+// }
+class Solution {
+  characterReplacement(s: string, k: number): number {
+    let count: { [key: string]: number } = {};
+    let res = 0;
+
+    let l = 0;
+    for (let r = 0; r < s.length; r++) {
+      count[s[r]] = (count[s[r]] || 0) + 1;
+
+      while (r - l + 1 - Math.max(...Object.values(count)) > k) {
+        count[s[l]]--;
+        l++;
+      }
+
+      res = Math.max(res, r - l + 1);
+    }
+    return res;
+  }
+}
+
+function characterReplacement(s: string, k: number): number {
+  return new Solution().characterReplacement(s, k);
+}
+
 
 console.log(characterReplacement("ABAB", 2));
