ad652245739dffbe38794dd9b7e01858ecba6d2e
macluiggy <luiggy.macias@imagineapps.co>
2024-07-15 18:07:33 -0500

    Refactor isSubtree function for improved performance and readability

---
+++ leetcode/70-blind-leetcode/tree/isSubtree.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+import TreeNode from "./TreeNode";
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if (!p && !q) return true;
+  if (!q || !p) return false;
+  if (q.val != p.val) return false;
+  return isSameTree(p?.left, q?.left) && isSameTree(p?.right, q?.right);
+}
+
+function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+  if (!root) return false;
+  if (isSameTree(root, subRoot)) return true;
+  return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+}
