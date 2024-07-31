f30956926df6ec0171ad1427d444806c29f77391
macluiggy <luiggy.macias@imagineapps.co>
2024-07-31 12:18:26 -0500

    chore: Implement binary search algorithm

---
+++ leetcode/pareto-problem-set/binary-search/search.ts
@@ -0,0 +1,18 @@
+function search(nums: number[], target: number): number {
+  let l = 0;
+  let r = nums.length - 1;
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2);
+    if (nums[mid] > target) {
+      r = mid - 1;
+    } else if (nums[mid] < target) {
+      l = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return -1;
+}
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9));
